77bd9e83ada690181967dea3ca1a33f4aa559956
ImNotTheGuy <lraulin843@gmail.com>
2024-05-27 17:21:25 +0200

    feat:trying custom environment variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
 import { StompSessionProvider } from 'react-stomp-hooks';
 import Main from './components/Main';
 import NoWebSocketConnexion from './components/NoWebSocketConnexion';
-import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [backendURL, setBackendURL] = useState(''); 
-
-  useEffect(() => {
-      fetch('/env.js')
-          .then(response => response.json())
-          .then(data => setBackendURL(data.REACT_APP_BACKEND_URL));
-  }, []);
-
-  console.log("Welcome!", backendURL); 
-
-  if (backendURL !== null) {
+  let url = null;
+  if (process.env.REACT_APP_BACKEND_URL) {
+    url = process.env.REACT_APP_BACKEND_URL;
+  }
     return (
-      <StompSessionProvider url={backendURL}>
+      <StompSessionProvider url={'https://plants-back-e7ciaosr2q-ey.a.run.app'}>
         <Main />
       </StompSessionProvider>
     )
-  } else {
-    return (
-      <NoWebSocketConnexion/>
-    )
   }
-}
 
 export default App;
