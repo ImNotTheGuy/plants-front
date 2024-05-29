a38857c652dd2d97ebc9fdbfbeb733d414c437fa
ImNotTheGuy <lraulin843@gmail.com>
2024-05-29 13:28:12 +0200

    fix?

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import { StompSessionProvider } from 'react-stomp-hooks';
 import Main from './components/Main';
+import { useState } from 'react';
 
 function App() {
 
-  const url = 'https://10.0.0.16:8080/ws-endpoint'
+  const [url, setUrl] = useState('https://10.0.0.16:8080/ws-endpoint');
+  const [newUrl, setNewUrl] = useState(url);
 
-  console.log("connecting to", url)
+  const handleChangeUrl = () => {
+    setUrl(newUrl);
+  }
 
-    return (
+  return (
+    <div className='min-h-screen bg-gradient-to-b from-green-100 to-green-200 p-8'>
+      <input
+        className='bg-white rounded-lg shadow-lg p-2 w-1/3 mx-4'
+        type='text'
+        value={newUrl}
+        onChange={(e) => setNewUrl(e.target.value)}
+        placeholder="Enter new URL"
+      />
+      <button className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg w-1/3' onClick={handleChangeUrl}>Change URL</button>
       <StompSessionProvider url={url}>
         <Main />
       </StompSessionProvider>
-    )
-  }
+    </div>
+  )
+}
 
 export default App;
