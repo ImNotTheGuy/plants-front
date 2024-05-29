5bba310115ac8734a82b8202e6a993af3b50acef
ImNotTheGuy <lraulin843@gmail.com>
2024-05-29 10:01:32 +0200

    fix ???

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     url = process.env.REACT_APP_BACKEND_URL;
   }
     return (
-      <StompSessionProvider url={'https://plants-back-e7ciaosr2q-ey.a.run.app:8080/ws-endpoint'}>
+      <StompSessionProvider url={'http://10.10.0.0:8080/ws-endpoint'}>
         <Main />
       </StompSessionProvider>
     )
